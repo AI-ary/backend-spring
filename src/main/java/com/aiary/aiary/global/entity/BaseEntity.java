448569585ac1113d6c75bfca2011e9814a456b24
Heekyeong Park <qkrgmlrud00@gmail.com>
2023-07-18 20:56:15 +0900

    feat: baseEntity로 공통 필드 분리

---
+++ src/main/java/com/aiary/aiary/global/entity/BaseEntity.java
@@ -0,0 +1,26 @@
+package com.aiary.aiary.global.entity;
+
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@Getter
+@EntityListeners(value = {AuditingEntityListener.class})
+@MappedSuperclass
+public abstract class BaseEntity {
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+    @Column(name = "is_deleted")
+    private boolean isDeleted;
+}
+
