9b1e3d213f702ae93e626d393a96e084cdbbaa05
Heekyeong Park <qkrgmlrud00@gmail.com>
2023-07-18 20:55:10 +0900

    chore: 초기 개발 환경 세팅

---
+++ src/main/java/com/aiary/aiary/AiaryApplication.java
@@ -0,0 +1,13 @@
+package com.aiary.aiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiaryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aiary/aiary/AiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aiary.aiary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
