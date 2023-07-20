f88bf9180879335655454ad51ee658cac3a4cff3
GaBaljaintheroom <junsu1222@naver.com>
2023-07-20 18:16:12 +0900

    refactor: Weather enum타입 설정 변경

--- src/main/java/com/aiary/aiary/domain/diary/entity/Weather.java
+++ src/main/java/com/aiary/aiary/domain/diary/entity/Weather.java
@@ -1,19 +1,5 @@
 package com.aiary.aiary.domain.diary.entity;
 
 public enum Weather {
-
-    SUNNY("맑음"),
-    CLOUDY("흐림"),
-    RAIN("비"),
-    SNOW("눈");
-
-    private final String type;
-
-    Weather(String type) {
-        this.type = type;
-    }
-
-    public String getType(){
-        return type;
-    }
+    SUNNY, CLOUDY, RAIN, SNOW;
 }
