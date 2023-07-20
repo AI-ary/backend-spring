531b98f6f15b9cb92236cb2c8e74e7a874fab843
Heekyeong Park <qkrgmlrud00@gmail.com>
2023-07-20 17:57:20 +0900

    feat: User Entity 구현

---
+++ src/main/java/com/aiary/aiary/domain/user/entity/User.java
@@ -0,0 +1,50 @@
+package com.aiary.aiary.domain.user.entity;
+
+
+import com.aiary.aiary.domain.diary.entity.Diary;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "Users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 100)
+    private String email;
+
+    @Column(nullable = false, length = 30)
+    private String nickname;
+
+    @Column(nullable = false, length = 250)
+    private String password;
+
+    @Column(nullable = false, length = 500)
+    private String thema;   // 테마 (변경될 수 있음)
+
+    @Column(name = "is_active")
+    private boolean isActive = false; // 활성 여부
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Diary> diaries = new ArrayList<>();
+
+    @Builder
+    private User(String email, String nickname, String password, String thema, boolean isActive) {
+        this.email = email;
+        this.nickname = nickname;
+        this.password = password;
+        this.thema = thema;
+        this.isActive = isActive;
+    }
+}
