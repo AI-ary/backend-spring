ec3070c15694e11e924483c276246558fbb2125d
GaBaljaintheroom <junsu1222@naver.com>
2023-07-26 01:10:40 +0900

    feat: CreateRequestToEntity Mapper 작성

---
+++ src/main/java/com/aiary/aiary/domain/diary/dto/mapper/DiaryMapper.java
@@ -0,0 +1,23 @@
+package com.aiary.aiary.domain.diary.dto.mapper;
+
+import com.aiary.aiary.domain.diary.dto.request.DiaryCreateRequest;
+import com.aiary.aiary.domain.diary.entity.Diary;
+import com.aiary.aiary.domain.diary.entity.Weather;
+import com.aiary.aiary.domain.user.entity.User;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DiaryMapper {
+
+    public Diary mapCreateRequestToEntity(DiaryCreateRequest diaryCreateRequest, User user){
+        return Diary.builder()
+                .title(diaryCreateRequest.getTitle())
+                .contents(diaryCreateRequest.getContents())
+                .weather(Weather.valueOf(diaryCreateRequest.getWeather()))
+                .emoji(diaryCreateRequest.getEmoji())
+                .drawingUrl(diaryCreateRequest.getDrawingUrl())
+                .diaryDate(diaryCreateRequest.getDiaryDate())
+                .user(user)
+                .build();
+    }
+}
--- src/main/java/com/aiary/aiary/domain/diary/service/DiaryService.java
+++ src/main/java/com/aiary/aiary/domain/diary/service/DiaryService.java
@@ -1,5 +1,6 @@
 package com.aiary.aiary.domain.diary.service;
 
+import com.aiary.aiary.domain.diary.dto.mapper.DiaryMapper;
 import com.aiary.aiary.domain.diary.dto.request.DiaryCreateRequest;
 import com.aiary.aiary.domain.diary.entity.Diary;
 import com.aiary.aiary.domain.diary.entity.Weather;
@@ -7,28 +8,22 @@ import com.aiary.aiary.domain.diary.exception.DiaryNotFoundException;
 import com.aiary.aiary.domain.diary.repository.DiaryRepository;
 import com.aiary.aiary.domain.user.entity.User;
 import com.aiary.aiary.domain.user.repository.UserRepository;
+import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
-@RequiredArgsConstructor
+@RequiredArgsConstructor(access = AccessLevel.PROTECTED)
 public class DiaryService {
 
     private final DiaryRepository diaryRepository;
     private final UserRepository userRepository ;
+    private final DiaryMapper diaryMapper;
 
     public void createDiary(DiaryCreateRequest diaryCreateRequest){
-        User curUser = userRepository.findById((long) diaryCreateRequest.getUserId()).orElseThrow(DiaryNotFoundException::new);
+        User findUser = userRepository.findById((long) diaryCreateRequest.getUserId()).orElseThrow(IllegalAccessError::new);
 
-        Diary newDiary = Diary.builder()
-                .title(diaryCreateRequest.getTitle())
-                .contents(diaryCreateRequest.getContents())
-                .weather(Weather.valueOf(diaryCreateRequest.getWeather()))
-                .emoji(diaryCreateRequest.getEmoji())
-                .drawingUrl(diaryCreateRequest.getDrawingUrl())
-                .diaryDate(diaryCreateRequest.getDiaryDate())
-                .user(curUser)
-                .build();
+        Diary newDiary = diaryMapper.mapCreateRequestToEntity(diaryCreateRequest, findUser);
         diaryRepository.save(newDiary);
     }
 }
