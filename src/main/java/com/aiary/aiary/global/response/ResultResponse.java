70f94b1d02aeff0a6225641093076da74d79d4a8
GaBaljaintheroom <junsu1222@naver.com>
2023-07-26 01:12:33 +0900

    refactor: ResultResponse 타입 수정

--- src/main/java/com/aiary/aiary/global/response/ResultResponse.java
+++ src/main/java/com/aiary/aiary/global/response/ResultResponse.java
@@ -1,23 +1,24 @@
 package com.aiary.aiary.global.response;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Getter
-@AllArgsConstructor
-@NoArgsConstructor
 public class ResultResponse {
 
     private String code;
     private String message;
+    private Object data;
 
     public static ResultResponse of(ResultCode resultCode){
-        return new ResultResponse(resultCode);
+        return new ResultResponse(resultCode, "");
+    }
+    public static ResultResponse of(ResultCode resultCode, Object data) {
+        return new ResultResponse(resultCode, data);
     }
 
-    public ResultResponse(ResultCode resultCode){
+    public ResultResponse(ResultCode resultCode, Object data){
         this.code = resultCode.getCode();
         this.message = resultCode.getMessage();
+        this.data = data;
     }
 }
