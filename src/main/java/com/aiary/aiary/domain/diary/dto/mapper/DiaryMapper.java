d8781f36ef4e971189cc52e46ae4a897d22cffd2
GaBaljaintheroom <junsu1222@naver.com>
2023-07-27 21:07:32 +0900

    feat: DiaryInfo 변환 Mapper 작성

--- src/main/java/com/aiary/aiary/domain/diary/dto/mapper/DiaryMapper.java
+++ src/main/java/com/aiary/aiary/domain/diary/dto/mapper/DiaryMapper.java
@@ -1,10 +1,15 @@
 package com.aiary.aiary.domain.diary.dto.mapper;
 
 import com.aiary.aiary.domain.diary.dto.request.DiaryCreateRequest;
+import com.aiary.aiary.domain.diary.dto.response.DiaryInfo;
+import com.aiary.aiary.domain.diary.dto.response.MonthlyDiaryInfo;
 import com.aiary.aiary.domain.diary.entity.Diary;
 import com.aiary.aiary.domain.diary.entity.Weather;
 import com.aiary.aiary.domain.user.entity.User;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 public class DiaryMapper {
@@ -20,4 +25,19 @@ public class DiaryMapper {
                 .user(user)
                 .build();
     }
+
+    public DiaryInfo toEntity(Diary diary){
+        return DiaryInfo.builder()
+                .title(diary.getTitle())
+                .weather(diary.getWeather())
+                .emoji(diary.getEmoji())
+                .contents(diary.getContents())
+                .diaryDate(diary.getDiaryDate())
+                .drawingUrl(diary.getDrawingUrl())
+                .build();
+    }
+
+
+
+
 }
