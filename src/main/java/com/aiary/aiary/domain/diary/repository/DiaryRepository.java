2188068c3429ffd1fe7e88b30d07025e8ec6d969
GaBaljaintheroom <junsu1222@naver.com>
2023-07-27 20:59:42 +0900

    feat: 달별 Diary 조회 쿼리 작성

--- src/main/java/com/aiary/aiary/domain/diary/repository/DiaryRepository.java
+++ src/main/java/com/aiary/aiary/domain/diary/repository/DiaryRepository.java
@@ -2,8 +2,19 @@ package com.aiary.aiary.domain.diary.repository;
 
 import com.aiary.aiary.domain.diary.entity.Diary;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.sql.Date;
+import java.util.List;
 
 @Repository
 public interface DiaryRepository extends JpaRepository<Diary, Long> {
+
+    @Query("SELECT d FROM Diary d JOIN d.user u WHERE u.id = :userId " +
+            "AND d.isDeleted = false " +
+            "AND FUNCTION('DATE_FORMAT', d.diaryDate, '%Y-%m') = :diaryDate " +
+            "order by d.diaryDate desc")
+    List<Diary> findMonthlyDiaryByUserId(@Param("userId") Long userId, @Param("diaryDate") Date diaryDate);
 }
