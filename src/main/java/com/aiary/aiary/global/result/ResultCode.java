4ef353bafd36f36084866b7764a3ab8fdf173b77
Heekyeong Park <qkrgmlrud00@gmail.com>
2023-07-28 16:01:36 +0900

    feat: resultResponse 관리

---
+++ src/main/java/com/aiary/aiary/global/result/ResultCode.java
@@ -0,0 +1,23 @@
+package com.aiary.aiary.global.result;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+/** {행위}_{목적어}_{성공여부} message 는 동사 명사형으로 마무리 */
+@Getter
+@AllArgsConstructor
+public enum ResultCode {
+
+  // 도메인 별로 나눠서 관리(ex: User 도메인)
+  // user
+  USER_REGISTRATION_SUCCESS("U001", "사용자 등록 성공"),
+  USER_EMAIL_DUPLICATED("U002", "회원 아이디 중복"),
+  USER_EMAIL_NOT_DUPLICATED("U003", "회원 아이디 중복되지 않음"),
+  USER_LOGIN_SUCCESS("U004", "회원 로그인 성공"),
+  USER_LOGOUT_SUCCESS("U005", "회원 로그아웃 성공"),
+
+  ;
+
+  private final String code;
+  private final String message;
+}
