3f9e436b27fc5050f2525ff1dcc4fc42045de54e
GaBaljaintheroom <junsu1222@naver.com>
2023-07-31 23:32:07 +0900

    refactor: Diary ResponseDTO Snake 형식 JSON으로 변경

--- src/main/java/com/aiary/aiary/domain/diary/dto/response/DiaryInfo.java
+++ src/main/java/com/aiary/aiary/domain/diary/dto/response/DiaryInfo.java
@@ -1,12 +1,15 @@
 package com.aiary.aiary.domain.diary.dto.response;
 
 import com.aiary.aiary.domain.diary.entity.Weather;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.*;
 
 @Builder
 @Getter
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class DiaryInfo {
 
     private String title;
