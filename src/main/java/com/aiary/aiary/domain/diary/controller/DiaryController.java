0a204e2744fa6a6e3b333e93ffee31fed79c5c94
Heekyeong Park <qkrgmlrud00@gmail.com>
2023-08-15 19:58:20 +0900

    chore: swagger jwt 인증 추가

--- src/main/java/com/aiary/aiary/domain/diary/controller/DiaryController.java
+++ src/main/java/com/aiary/aiary/domain/diary/controller/DiaryController.java
@@ -5,6 +5,8 @@ import com.aiary.aiary.domain.diary.dto.request.DiaryCreateRequest;
 import com.aiary.aiary.domain.diary.service.DiaryService;
 import com.aiary.aiary.global.result.ResultCode;
 import com.aiary.aiary.global.result.ResultResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
@@ -15,6 +17,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.util.Date;
 
+@Tag(name = "Diary", description = "일기 API")
 @Controller
 @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 @RequestMapping("/api/diaries")
@@ -22,18 +25,21 @@ public class DiaryController {
 
     private final DiaryService diaryService;
 
+    @Operation(summary = "일기등록")
     @PostMapping
     public ResponseEntity<ResultResponse> createDiary(@Valid @RequestBody DiaryCreateRequest createRequest){
         diaryService.createDiary(createRequest);
         return ResponseEntity.ok(ResultResponse.of(ResultCode.DIARY_CREATE_SUCCESS));
     }
-    
+
+    @Operation(summary = "일기 id 별 조회")
     @DeleteMapping("/{diaryId}")
     public ResponseEntity<ResultResponse> deleteDiary(@PathVariable Long diaryId){
         diaryService.deleteDiary(diaryId);
         return ResponseEntity.ok(ResultResponse.of(ResultCode.DIARY_DELETE_SUCCESS));
     }
 
+    @Operation(summary = "일기 월 별 조회")
     @GetMapping
     public ResponseEntity<ResultResponse> findMonthlyDiary(@RequestParam("user_id") Long userId,
                                                            @RequestParam("diary_date")
