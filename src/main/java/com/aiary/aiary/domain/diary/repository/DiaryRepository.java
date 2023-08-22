80a2f5eddfa02fc8b519fa0187ec515c17f4c2d8
GaBaljaintheroom <junsu1222@naver.com>
2023-08-23 01:39:38 +0900

    feat: searchDiariesByKeyword repository 작성

--- src/main/java/com/aiary/aiary/domain/diary/repository/DiaryRepository.java
+++ src/main/java/com/aiary/aiary/domain/diary/repository/DiaryRepository.java
@@ -1,6 +1,8 @@
 package com.aiary.aiary.domain.diary.repository;
 
 import com.aiary.aiary.domain.diary.entity.Diary;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -18,4 +20,7 @@ public interface DiaryRepository extends JpaRepository<Diary, Long> {
             " = FUNCTION('DATE_FORMAT', :diaryDate, '%Y-%m')" +
             "order by d.diaryDate desc")
     List<Diary> findMonthlyDiaryByUserId(@Param("userId") Long userId, @Param("diaryDate") Date diaryDate);
+
+    @Query("SELECT d FROM Diary d JOIN d.user u WHERE u.id = :userId AND d.isDeleted = FALSE AND d.title LIKE %:keyword% OR d.contents LIKE %:keyword%")
+    Slice<Diary> searchDiariesByKeyword(@Param("userId") Long userId, Pageable pageable, @Param("keyword") String keyword);
 }
