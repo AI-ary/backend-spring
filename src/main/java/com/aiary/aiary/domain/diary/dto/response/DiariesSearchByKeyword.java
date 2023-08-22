efe13d1775c6e512dc77f5cf3fe5c5fab47881ce
GaBaljaintheroom <junsu1222@naver.com>
2023-08-23 01:38:34 +0900

    feat: DiariesSearchByKeyword DTO생성

---
+++ src/main/java/com/aiary/aiary/domain/diary/dto/response/DiariesSearchByKeyword.java
@@ -0,0 +1,20 @@
+package com.aiary.aiary.domain.diary.dto.response;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.*;
+
+import java.util.List;
+@Builder
+@Getter
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class DiariesSearchByKeyword {
+
+    private List<DiaryInfo> diaryInfos;
+    private int curPageNumber;
+    private boolean hasNext;
+    private boolean hasPrevious;
+
+}
