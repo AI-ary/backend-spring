c68320217dc1947b93556d45d351dd2b7bae53a7
GaBaljaintheroom <junsu1222@naver.com>
2023-09-03 01:26:46 +0900

    refactor: SnakeCaseStrategy.class 추가

--- src/main/java/com/aiary/aiary/global/jwt/JwtToken.java
+++ src/main/java/com/aiary/aiary/global/jwt/JwtToken.java
@@ -1,11 +1,14 @@
 package com.aiary.aiary.global.jwt;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.*;
 
 @Builder
 @Getter
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class JwtToken {
     private String grantType;
     private String accessToken;
