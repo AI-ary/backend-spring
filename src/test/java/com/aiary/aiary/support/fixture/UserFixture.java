7a3e63c42c283cf4fbde36e1ba400e42351149ae
GaBaljaintheroom <junsu1222@naver.com>
2023-09-20 21:48:57 +0900

    feat: DIARY_CREATE_USER 생성

--- src/test/java/com/aiary/aiary/support/fixture/UserFixture.java
+++ src/test/java/com/aiary/aiary/support/fixture/UserFixture.java
@@ -6,16 +6,25 @@ import com.aiary.aiary.domain.user.entity.UserDetail;
 
 public class UserFixture {
 
-    public static final User TEST_USER = User.builder()
-            .email("test@gmail.com")
+    public static final User DIARY_CREATE_USER = User.builder()
+            .email("Create@gmail.com")
             .password("Test012@")
             .nickname("테스트유저")
             .role(Role.USER)
             .build();
 
-    public static final UserDetail TEST_USERDETAIL = UserDetail.builder()
-            .user(TEST_USER)
+    public static final UserDetail DIARY_CREATE_USERDETAIL = UserDetail.builder()
+            .user(DIARY_CREATE_USER)
             .build();
 
+    public static final User DIARY_DELETE_USER = User.builder()
+            .email("Delete@gmail.com")
+            .password("Test012@")
+            .nickname("테스트유저")
+            .role(Role.USER)
+            .build();
 
+    public static final UserDetail DIAEY_DELETE_USERDETAIL = UserDetail.builder()
+            .user(DIARY_DELETE_USER)
+            .build();
 }
