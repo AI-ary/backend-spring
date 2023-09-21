e49799ffd48ceac0ef9c5e0efb736ecbc9bd9877
GaBaljaintheroom <junsu1222@naver.com>
2023-09-21 20:29:22 +0900

    feat: DiaryFixture InsertDiaries 작성

--- src/test/java/com/aiary/aiary/support/fixture/DiaryFixture.java
+++ src/test/java/com/aiary/aiary/support/fixture/DiaryFixture.java
@@ -5,6 +5,8 @@ import com.aiary.aiary.domain.diary.entity.Diary;
 import com.aiary.aiary.domain.diary.entity.Weather;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 public class DiaryFixture {
 
@@ -35,4 +37,32 @@ public class DiaryFixture {
             .contents("일기 내용")
             .diaryDate(LocalDate.of(2023,9,19))
             .build();
+
+    public static List<Diary> InsertDiaries() {
+        List<Diary> diaries = new ArrayList<>();
+        Diary diary1 = Diary.builder()
+                .title("일기 제목1")
+                .weather(Weather.SUNNY)
+                .emoji("\uD83D\uDCAA")
+                .contents("일기 내용1")
+                .drawingUrl("url1")
+                .diaryDate(LocalDate.of(2023,9,21))
+                .user(UserFixture.DIARY_FIND_MONTH_USER)
+                .build();
+
+        Diary diary2 = Diary.builder()
+                .title("일기 제목2")
+                .weather(Weather.SUNNY)
+                .emoji("\uD83D\uDCAA")
+                .contents("일기 내용2")
+                .drawingUrl("url1")
+                .diaryDate(LocalDate.of(2023,9,22))
+                .user(UserFixture.DIARY_FIND_MONTH_USER)
+                .build();
+
+        diaries.add(diary1);
+        diaries.add(diary2);
+
+        return diaries;
+    }
 }
