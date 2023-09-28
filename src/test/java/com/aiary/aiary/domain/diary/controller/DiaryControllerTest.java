3d51d6982405efb184a12d62db4b1b11a2166764
GaBaljaintheroom <junsu1222@naver.com>
2023-09-29 00:10:17 +0900

    test: 사용자가 작성한 일기를 달별로 조회할 수 있다.

--- src/test/java/com/aiary/aiary/domain/diary/controller/DiaryControllerTest.java
+++ src/test/java/com/aiary/aiary/domain/diary/controller/DiaryControllerTest.java
@@ -1,7 +1,10 @@
 package com.aiary.aiary.domain.diary.controller;
 
 import com.aiary.aiary.domain.diary.dto.request.DiaryCreateReq;
+import com.aiary.aiary.domain.diary.dto.response.DiaryRes;
+import com.aiary.aiary.domain.diary.dto.response.MonthlyDiaryRes;
 import com.aiary.aiary.domain.diary.service.DiaryService;
+import com.aiary.aiary.domain.user.entity.UserDetail;
 import com.aiary.aiary.global.s3.service.S3UploadService;
 import com.aiary.aiary.support.fixture.DiaryControllerFixture;
 import com.aiary.aiary.support.utils.MockApiTest;
@@ -12,20 +15,26 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.restdocs.payload.JsonFieldType;
 import org.springframework.test.web.servlet.ResultActions;
 
 import java.nio.charset.StandardCharsets;
+import java.util.List;
 
 import static com.aiary.aiary.support.docs.ApiDocumentUtils.getDocumentRequest;
 import static com.aiary.aiary.support.docs.ApiDocumentUtils.getDocumentResponse;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.doNothing;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.delete;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.multipart;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -88,7 +97,7 @@ class DiaryControllerTest extends MockApiTest {
     @DisplayName("사용자가 작성한 일기가 PK로 삭제될 수 있다.")
     void deleteDiary() throws Exception {
         //given
-        doNothing().when(diaryService).deleteDiary(any(), any());
+        doNothing().when(diaryService).deleteDiary(any(UserDetail.class), any(Long.class));
 
         //when
         ResultActions perform =
@@ -103,4 +112,43 @@ class DiaryControllerTest extends MockApiTest {
                         getDocumentRequest(),
                         getDocumentResponse()));
     }
+
+    @Test
+    @DisplayName("사용자가 작성한 일기를 달별로 조회할 수 있다.")
+    void getMonthlyDiary() throws Exception {
+        // given
+        String diaryDate = "2023-09";
+        List<DiaryRes> expectedDiaries = DiaryControllerFixture.MONTHLY_DIARIES();
+        MonthlyDiaryRes monthlyDiaryRes = MonthlyDiaryRes.builder()
+                .monthlyDiaryRes(expectedDiaries)
+                .build();
+        given(diaryService.findMonthlyDiaryByDate(any(UserDetail.class), eq(diaryDate))).willReturn(monthlyDiaryRes);
+
+        // when
+        ResultActions perform = mockMvc.perform(
+                get("/diaries")
+                        .param("diary_date", diaryDate)
+                        .contentType(MediaType.APPLICATION_JSON));
+
+        // then
+        perform.andExpect(status().isOk())
+                .andDo(print())
+                .andDo(document("get monthlyDiaries",
+                        getDocumentRequest(),
+                        getDocumentResponse(),
+                        responseFields(
+                                fieldWithPath("code").description("상태 코드"),
+                                fieldWithPath("message").description("응답 메시지"),
+                                fieldWithPath("data").description("응답 데이터").type(JsonFieldType.OBJECT).optional(),
+                                fieldWithPath("data.monthly_diary_info").description("월별 일기 정보").type(JsonFieldType.ARRAY).optional(),
+                                fieldWithPath("data.monthly_diary_info[].diary_id").type(JsonFieldType.NUMBER).description("일기 ID").optional(),
+                                fieldWithPath("data.monthly_diary_info[].title").type(JsonFieldType.STRING).description("일기 제목").optional(),
+                                fieldWithPath("data.monthly_diary_info[].weather").type(JsonFieldType.STRING).description("일기 날씨").optional(),
+                                fieldWithPath("data.monthly_diary_info[].emoji").type(JsonFieldType.STRING).description("이모지").optional(),
+                                fieldWithPath("data.monthly_diary_info[].contents").type(JsonFieldType.STRING).description("일기 내용").optional(),
+                                fieldWithPath("data.monthly_diary_info[].diary_date").type(JsonFieldType.STRING).description("일기 날짜").optional(),
+                                fieldWithPath("data.monthly_diary_info[].drawing_url").type(JsonFieldType.STRING).description("일기 그림 URL").optional())
+                ));
+    }
+
 }
--- src/test/java/com/aiary/aiary/support/fixture/DiaryControllerFixture.java
+++ src/test/java/com/aiary/aiary/support/fixture/DiaryControllerFixture.java
@@ -1,8 +1,13 @@
 package com.aiary.aiary.support.fixture;
 
 import com.aiary.aiary.domain.diary.dto.request.DiaryCreateReq;
+import com.aiary.aiary.domain.diary.dto.response.DiaryRes;
+import com.aiary.aiary.domain.diary.entity.Weather;
 
 import java.time.LocalDate;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class DiaryControllerFixture {
 
@@ -13,4 +18,18 @@ public class DiaryControllerFixture {
             .weather("SUNNY")
             .emoji("\uD83D\uDCAA")
             .build();
+
+    public static List<DiaryRes> MONTHLY_DIARIES() {
+        return IntStream.range(0, 5)
+                .mapToObj(i -> DiaryRes.builder()
+                        .diaryId((long) i)
+                        .title("일기 제목" + i)
+                        .weather(Weather.SUNNY)
+                        .emoji("\uD83D\uDCAA")
+                        .contents("일기 내용" + i)
+                        .drawingUrl("url" + i)
+                        .diaryDate("2023-09-0" + (i + 1))
+                        .build())
+                .collect(Collectors.toList());
+    }
 }
